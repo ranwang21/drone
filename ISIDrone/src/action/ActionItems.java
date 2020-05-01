57a71ab7b35770d381ad552018baba68b9da0379
rick4realgh <51961646+rick4realgh@users.noreply.github.com>
2020-05-01 09:25:30 -0400

    ISID-10 Affichage de la liste des produits a partir de la recherche

--- ISIDrone/src/action/ActionItems.java
+++ ISIDrone/src/action/ActionItems.java
@@ -6,13 +6,17 @@ import javax.servlet.http.HttpServletResponse;
 import manager.MItem;
 
 public class ActionItems {
-	
-	public static void getItems(HttpServletRequest request, HttpServletResponse response) {
-		request.setAttribute("items", MItem.getItems(ActionCategory.getSelectedCategory(request, response)));
-	}
 
-	public static void getItemById(int id, HttpServletRequest request, HttpServletResponse response) {
-		request.setAttribute("item", MItem.getItemById(id));
-	}
-	
+    public static void getItems(HttpServletRequest request, HttpServletResponse response) {
+        request.setAttribute("items", MItem.getItems(ActionCategory.getSelectedCategory(request, response)));
+    }
+
+    public static void getSearchItems(HttpServletRequest request, HttpServletResponse response) {
+        request.setAttribute("items", MItem.getSearchItems(request.getParameter("search")));
+    }
+
+    public static void getItemById(int id, HttpServletRequest request, HttpServletResponse response) {
+        request.setAttribute("item", MItem.getItemById(id));
+    }
+
 }
