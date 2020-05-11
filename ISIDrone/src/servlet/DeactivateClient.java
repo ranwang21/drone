ef4fde301893266bb3240732883eabcd22298f69
ran wang <wang.ran.henri@gmail.com>
2020-05-11 14:59:26 -0400

    Add securisation AddProduct, Deactivate User, et deleteProduct

--- ISIDrone/src/servlet/DeactivateClient.java
+++ ISIDrone/src/servlet/DeactivateClient.java
@@ -2,6 +2,7 @@ package servlet;
 
 import action.ActionCategory;
 import action.ActionUser;
+import entities.User;
 import util.Const;
 
 import javax.servlet.ServletException;
@@ -27,14 +28,22 @@ public class DeactivateClient extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        int idClient = Integer.parseInt(request.getParameter("user_id"));
-        // return true if deactivated
-        boolean isDeleted = ActionUser.Deactivate(idClient);
-        if (!isDeleted) {
-            request.setAttribute("deleteError", "Delete Error");
+        User user = (User) request.getSession().getAttribute("user");
+
+        if (user != null && user.getIsAdmin() == 1) {
+            int idClient = Integer.parseInt(request.getParameter("user_id"));
+            // return true if deactivated
+            boolean isDeleted = ActionUser.Deactivate(idClient);
+            if (!isDeleted) {
+                request.setAttribute("deleteError", "Delete Error");
+            }
+            ActionUser.getClients(request, response);
+            request.getRequestDispatcher(Const.PATH_PAGE_LIST_CLIENTS).forward(request, response);
+        } else {
+            response.sendRedirect(Const.PATH_REDIRECT_HOME);
         }
-        ActionUser.getClients(request, response);
-        request.getRequestDispatcher(Const.PATH_PAGE_LIST_CLIENTS).forward(request, response);
+
+
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
